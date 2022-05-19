b3110c1dd0390df21d0ee2c5bf52daf5d7662dbe
termissues <termissues@gmail.com>
2022-05-19 10:25:53 +0300

    New push

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import NewForm from "./pages/NewForm";
 import Details from "./pages/Details";
 import ListPage from "./pages/List";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return <div>
 
-    <Router>
-    <Routes>
-    <Route path='/' element={<ListPage/>} />
-      <Route path='/newformpage' element={<NewForm/>} />
-      <Route path='/detailspage' element={<Details/>} />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route exact path='/' element={<ListPage />} />
+        <Route path='/newformpage' element={<NewForm />} />
+        <Route path='/detailspage' element={<Details />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </div>;
 }
 
